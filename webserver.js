358f831925c56ea31f0c18ed9008847f322486d1
Matthew Taylor <rhyolight@gmail.com>
2014-03-17 11:17:19 -0700

    Using connect simple server.
    - Moved static files into 'static' directory.
    - Updated webserver to use connect for easy static file serving.

--- webserver.js
+++ webserver.js
@@ -1,65 +1,39 @@
 // A very basic web server in node.js
 // Stolen from: Node.js for Front-End Developers by Garann Means (p. 9-10) 
- 
-var port = 8000;
-var serverUrl = "127.0.0.1";
- 
-var http = require("http");
-var path = require("path");
-var fs = require("fs");
 
-function getFile(localPath, res, mimeType) {
-	fs.readFile(localPath, function(err, contents) {
-		if(!err) {
-			res.setHeader("Content-Length", contents.length);
-			res.setHeader("Content-Type", mimeType);
-			res.statusCode = 200;
-			res.end(contents);
-		} else {
-			res.writeHead(500);
-			res.end();
-		}
-	});
+// Dependencies.
+var path = require("path");
+var connect = require("connect");
+// Constants.
+var PORT = 8000;
+var SERVER_URL = "127.0.0.1";
+var VALID_EXTENSIONS = [
+	".html", ".js", ".css", ".txt", ".jpg", ".gif", ".png", ".ico", ".json"
+];
+// Connect application.
+var app;
+
+function isValidExtension(extension) {
+	return VALID_EXTENSIONS.indexOf(ext) > -1;
 }
 
-console.log("Starting web server at " + serverUrl + ":" + port);
- 
-http.createServer( function(req, res) {
- 
-	var now = new Date();
- 
+function filterExtensions(req, res, next) {
 	var filename = req.url || "index.html";
 	var ext = path.extname(filename);
-	var localPath = __dirname;
-	var validExtensions = {
-		".html" : "text/html",			
-		".js": "application/javascript", 
-		".css": "text/css",
-		".txt": "text/plain",
-		".jpg": "image/jpeg",
-		".gif": "image/gif",
-		".png": "image/png",
-		".ico": "image/png",
-		".json": "application/json"
-	};
-	var isValidExt = validExtensions[ext];
- 
-	if (isValidExt) {
-		
-		localPath += filename;
-		path.exists(localPath, function(exists) {
-			if(exists) {
-				console.log("Serving file: " + localPath);
-				getFile(localPath, res, ext);
-			} else {
-				console.log("File not found: " + localPath);
-				res.writeHead(404);
-				res.end();
-			}
-		});
- 
+	if (isValidExtension) {
+		// Pass control to next middleware.
+		next();
 	} else {
-		console.log("Invalid file extension detected: " + ext)
+		res.statusCode = 404;
+		res.end("File not found.");
 	}
- 
-}).listen(port, serverUrl);
+}
+
+app = connect()
+	// Ignore requests for files outside valid extensions.
+	.use(filterExtensions)
+	// Server all files from 'static' directory.
+	.use(connect.static(__dirname + '/static'));
+
+console.log("Starting web server at " + SERVER_URL + ":" + PORT);
+app.listen(PORT, SERVER_URL);
